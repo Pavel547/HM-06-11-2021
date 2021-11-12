85d6073fdead2906bdb5624a947bdd3243248675
Pavel Yevdokymenko <81463574+Pavel547@users.noreply.github.com>
2021-11-12 18:09:49 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//const a =[0,1,2,3,4,5,6,7,8,9]
+//const b = a.slice(4, 6)
+//console.log("a=", a,"b=", b);
+
+// const a =[0,1,2,3,4,5,6,7,8,9]
+// const b = a.splice(5, 3)
+// console.log("a=", a,"b=", b);
+
+//Завданя перше, друге та третє я не зрозумів
+
+// const a = [0, 1, false, 2, undefined, '', 3, null]
+// for (let numder of a ){
+//     if(!numder){
+//         continue;
+//     };
+//     console.log(numder);
+// };
+
